66a433b36828e97e07c3411eb7da24934f454e4c
Howard Hsu <howard@playmore.com.hk>
2023-12-08 17:17:37 +0800

    modlify layout
    modlify layout

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -15,14 +15,20 @@ import { airTable } from "../../services/AirTable";
 function NavBar() {
   const navigate = useNavigate();
   const [navigationData, setNavigationData] = useState([]);
+  const [logo, setLogo] = useState("");
 
   useEffect(() => {
+    async function getLogo() {
+      const response = await airTable("logo", "data");
+      setLogo(response[0].fields.img);
+    }
+
     async function getNavigation() {
-      const response = await airTable("導覽列", "data");
-      console.log("navigationData", response);
+      const response = await airTable("navigation", "data");
       setNavigationData(response);
     }
 
+    getLogo();
     getNavigation();
   }, []);
 
@@ -35,26 +41,27 @@ function NavBar() {
               navigate("/");
             }}
           >
-            <img
-              src="https://imgur.com/vG5oafk.png"
-              width="150"
-              height="50"
-              className="d-inline-block align-top"
-              alt="logo"
-            />
+            {logo !== "" && (
+              <img
+                src={logo}
+                className={`${css.logoView} d-inline-block align-top`}
+                alt="logo"
+              />
+            )}
           </Navbar.Brand>
           <Navbar.Toggle aria-controls={`offcanvasNavbar-expand`} />
           <Navbar.Offcanvas
             id={`offcanvasNavbar-expand`}
             aria-labelledby={`offcanvasNavbarLabel-expand`}
             placement="start"
+            className={css.offcanvasView}
           >
             <Offcanvas.Body>
               <Nav className="justify-content-start flex-grow-1 pe-3">
                 {navigationData.map((item, index) => (
                   <Nav.Link
                     key={index}
-                    className={`d-flex justify-content-center`}
+                    className={`${css.navLinkText} d-flex justify-content-center`}
                     onClick={() => {
                       navigate(item.fields.path);
                     }}
@@ -64,10 +71,14 @@ function NavBar() {
                 ))}
               </Nav>
               <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link className={`d-flex justify-content-center`}>
+                <Nav.Link
+                  className={`${css.navLinkText} d-flex justify-content-center`}
+                >
                   商店介紹
                 </Nav.Link>
-                <Nav.Link className={`d-flex justify-content-center`}>
+                <Nav.Link
+                  className={`${css.navLinkText} d-flex justify-content-center`}
+                >
                   聯絡我們
                 </Nav.Link>
               </Nav>
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import css from "./homepage.module.css";
+import { useNavigate, useParams } from "react-router-dom";
 import Layout from "../../components/CustomLayout/Layout";
 import { airTable } from "../../services/AirTable";
 
 function Home() {
+  let { id } = useParams();
   return (
     <Layout>
       <div>Home</div>
