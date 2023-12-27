8081dfc6b413f48197ac603c563bbba581dda639
Howard Hsu <howard@playmore.com.hk>
2023-12-27 15:59:58 +0800

    modlify setting
    modlify setting

--- src/index.js
+++ src/index.js
@@ -8,11 +8,9 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = document.getElementById("root");
 ReactDOM.createRoot(root).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename="/src">
+    <App />
+  </BrowserRouter>
 );
 
 reportWebVitals();
