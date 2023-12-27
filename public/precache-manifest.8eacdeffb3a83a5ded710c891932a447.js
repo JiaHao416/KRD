5050c0003ba2f683f64a1266ea7d0cfee368ac5c
Howard Hsu <howard@playmore.com.hk>
2023-12-27 15:25:07 +0800

    modlify setting
    modlify setting

---
+++ public/precache-manifest.8eacdeffb3a83a5ded710c891932a447.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffb29baf466e55e3072a",
+    "url": "/static/css/main.02b0d392.chunk.css"
+  },
+  {
+    "revision": "ffb29baf466e55e3072a",
+    "url": "/static/js/main.136cb4b3.chunk.js"
+  },
+  {
+    "revision": "468206d6331b684988b5",
+    "url": "/static/js/runtime~main.82e7454a.js"
+  },
+  {
+    "revision": "bbc488b809308d6bffb0",
+    "url": "/static/css/2.51b11787.chunk.css"
+  },
+  {
+    "revision": "bbc488b809308d6bffb0",
+    "url": "/static/js/2.f285a050.chunk.js"
+  },
+  {
+    "revision": "81359f9f9155fbe072fc",
+    "url": "/static/js/3.85df28a4.chunk.js"
+  },
+  {
+    "revision": "523aebf4eb18a3c52a82bd1ceca97edc",
+    "url": "/index.html"
+  }
+];
