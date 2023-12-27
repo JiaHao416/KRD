c0e4b6bc4234b959bcb0b3219aadb81a164740a6
Howard Hsu <howard@playmore.com.hk>
2023-12-27 16:39:33 +0800

    modlify setting
    modlify setting

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = document.getElementById("root");
 ReactDOM.createRoot(root).render(
-  <BrowserRouter basename="/src">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
