0fecb547552cf5778c83d70d834d8f6457c29ef4
Howard <howard880416@gmail.com>
2024-02-19 17:34:31 +0800

    modlify public
    modlfiy public

---
+++ public/precache-manifest.57756e56b1ce64c087dbef10cc6f6dbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9554427975a94e49b762",
+    "url": "/static/css/main.096da665.chunk.css"
+  },
+  {
+    "revision": "9554427975a94e49b762",
+    "url": "/static/js/main.fb5e3e93.chunk.js"
+  },
+  {
+    "revision": "468206d6331b684988b5",
+    "url": "/static/js/runtime~main.82e7454a.js"
+  },
+  {
+    "revision": "a98a58643cd6dc586f89",
+    "url": "/static/css/2.0cd23d53.chunk.css"
+  },
+  {
+    "revision": "a98a58643cd6dc586f89",
+    "url": "/static/js/2.d8f0becb.chunk.js"
+  },
+  {
+    "revision": "81359f9f9155fbe072fc",
+    "url": "/static/js/3.85df28a4.chunk.js"
+  },
+  {
+    "revision": "698ce4df1a3b23f0dc6613cb1bc3ba69",
+    "url": "/index.html"
+  }
+];
