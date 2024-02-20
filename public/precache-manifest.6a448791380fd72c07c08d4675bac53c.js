2340c8745650cca852afb1cbd7dafdb27bdad288
Howard <howard880416@gmail.com>
2024-02-20 17:02:58 +0800

    update

---
+++ public/precache-manifest.6a448791380fd72c07c08d4675bac53c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    revision: "c3217ede9d7780aaf9e9",
+    url: "/static/css/main.096da665.chunk.css",
+  },
+  {
+    revision: "c3217ede9d7780aaf9e9",
+    url: "/static/js/main.0ef4d740.chunk.js",
+  },
+  {
+    revision: "468206d6331b684988b5",
+    url: "/static/js/runtime~main.82e7454a.js",
+  },
+  {
+    revision: "a98a58643cd6dc586f89",
+    url: "/static/css/2.0cd23d53.chunk.css",
+  },
+  {
+    revision: "a98a58643cd6dc586f89",
+    url: "/static/js/2.d8f0becb.chunk.js",
+  },
+  {
+    revision: "81359f9f9155fbe072fc",
+    url: "/static/js/3.85df28a4.chunk.js",
+  },
+  {
+    revision: "8b5a17b31a54fed985e3dbfe09866920",
+    url: "/",
+  },
+];
